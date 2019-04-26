6f75027d9a489fd791077457c811f62e3d1f9f84
amarq <amarq@ADRIANO>
2019-04-26 01:24:04 -0400

    Salva todos aquivos .dot em um único arquivo .csv, a partir do result…  …

--- parser.py
+++ parser.py
@@ -2,66 +2,67 @@
 # -*- coding: utf-8 -*-
 import re 
 import csv
+
+filepath = "C:\Codigos\Engenharia_Software"
+dirname = filepath.split("\\")[-1]
+
+with open(str(dirname)+".csv", 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+        spamwriter.writerow(['DIR','CHAMADOR', ' CHAMADO']) # escreve st
 y = 1
+while (True):
+    if y is 17:
+        break
 
-with open('gen.csv', 'w', newline='') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=' ',
-                                quotechar=',', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(['CHAMADOR', ' CHAMADO']) # escreve st
+    relation = {
+        'Node': {
 
-while (y <= 16):
-    print ('entrei no while')
+        },
+        'correlacao': [
+            
+        ]
+    }
 
     arq = open(str(y)+'gen.dot', 'r')
     texto2 = arq.readlines()
 
-    testedict = {
-        'Node': {
-
-        },
-        'cor': [
-            
-        ]
-    }
     #separa chamador
     for linha in texto2 :
         chamado = re.search(r'(Node\d+).*label=\"(.*?)\"', linha)
         if chamado:
-            testedict['Node'].update(dict({chamado.group(1): {}}))
-            testedict['Node'][chamado.group(1)].update({'chamado': []})
-            testedict['Node'][chamado.group(1)].update({'label' : chamado.group(2)})
+            relation['Node'].update(dict({chamado.group(1): {}}))
+            relation['Node'][chamado.group(1)].update({'chamado': []})
+            relation['Node'][chamado.group(1)].update({'label' : chamado.group(2)})
 
     #separa chamados
     for linha in texto2 :
         relacao = re.search(r'(Node\d+).*-> (Node\d+)', linha)
 
         if relacao:
-            if relacao.group(1) in testedict['Node']:
-                testedict['Node'][relacao.group(1)]['chamado'].append(relacao.group(2))
+            if relacao.group(1) in relation['Node']:
+                relation['Node'][relacao.group(1)]['chamado'].append(relacao.group(2))
     arq.close()
     print('==================================================')
 
     #concatenas as relações chamador com chamados
-    for nodes in testedict['Node']:
+    for nodes in relation['Node']:
         chamado = []
-        for x in testedict['Node'][nodes]['chamado']:
-            chamado.append(testedict['Node'][x]['label'])
-        testedict['cor'].append(tuple((testedict['Node'][nodes]['label'], chamado)))
+        for x in relation['Node'][nodes]['chamado']:
+            chamado.append(relation['Node'][x]['label'])
+        relation['correlacao'].append(tuple((relation['Node'][nodes]['label'], chamado)))
 
-    z=int(0) 
-    for x in testedict ['cor']:
-        if z < int(1):
+    z = 0 
+    for x in relation ['correlacao']:
+        if z < 1:
             print(x[0],x[1])
-            z+=1
-#    print (testedict['cor'])
+            z += 1
 
     print ("\n ")
 # salvando no arvivo csv   
-    with open('gen.csv', 'a', newline='') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar=' ', quoting=csv.QUOTE_MINIMAL)
-        for x in testedict ['cor']:
+    with open(str(dirname)+".csv", 'a', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+        for x in relation ['correlacao']:
             for k in x[1]:
-                spamwriter.writerow([x[0],',',k])
+                spamwriter.writerow([dirname,x[0],k])
             break
     y += 1
